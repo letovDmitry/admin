a51d89135b49b5dba7358a04ae2f047ffe04fe78
letovDmitry <67585149+letovDmitry@users.noreply.github.com>
2024-07-31 15:18:47 +0600

    admin

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react"
 const Homepage = () => {
   const router = useRouter()
   useEffect(() => {
-    router.push("/admin")
+    router.push("/admin/login")
   }, [])
   return (
     <div>Homepage</div>
